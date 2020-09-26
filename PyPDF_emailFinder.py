b9ad56b9d4cebd12d4b54970b04a6085e3ee67aa
Satyam06 <satyampandey.58@gmail.com>
2020-09-26 20:06:13 +0200

    Add files via upload

---
+++ PyPDF_emailFinder.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 07:38:55 2020
+
+@author: Satyam
+"""
+
+#pip install PyPDF2
+import PyPDF2
+import os
+import re
+
+
+path = r'D://Research Thesis//Papers//IEEE SAM 2020 Papers'
+#direc = os.listdir(path)
+#print(len(direc))
+for zz in (os.listdir(path)):
+    #if filename.endswith(".pdf") or filename.endswith(".png"):
+    if zz.endswith(".pdf"):
+        name = os.path.join(path, zz)
+        print(name)
+        file = open(name, 'rb') 
+        pdfReader = PyPDF2.PdfFileReader(file)
+        pages = pdfReader.numPages 
+        for i in range(pages):   
+            page = pdfReader.getPage(i)   
+            print("Page no:",i)
+            #print(page.extractText())
+            value= page.extractText()        
+            emails = re.findall("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", value)
+            #values = re.findall('\S+@\S+', value)
+            print(emails)
+            break
